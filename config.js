95e18b92f1bb482a946195f357bcd0be7713a01e
Prasannakumara <153364276+Prasannakumara@users.noreply.github.com>
2025-01-04 22:10:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/6WRZcsB/eb0ef03007b9a177.jpg",
+ALIVE_MSG: process.env.ALIVE_IMG || "HELLO, I am prasanna kumara, I am alive now",
 };
+
